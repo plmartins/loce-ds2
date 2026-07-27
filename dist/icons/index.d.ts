73e436224a5491182ae32e0d385d8bf38360d7ec
Paulo Martins <jp-zeza@hotmail.com>
2026-07-27 16:08:21 -0400

    release v0.10.0 (dist)

---
+++ dist/icons/index.d.ts
@@ -0,0 +1,363 @@
+import { I as IconProps } from '../create-icon-D01KeTmq.js';
+import * as react from 'react';
+import * as _phosphor_icons_react from '@phosphor-icons/react';
+
+declare const IconDashboard: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCatalog: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSales: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconLogistics: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPeople: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconFinance: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconTasks: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconIntegrations: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSettings: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconProducts: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconClients: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSupplier: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCourier: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconStore: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCompany: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconWallet: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMoney: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCard: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCommission: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconVault: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconTag: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPromotion: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconInvoice: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconReceipt: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPrinter: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconQrCode: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCart: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconAddress: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconGift: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconNotification: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMail: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPhone: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconLink: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconGlobe: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconShield: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconKey: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconLock: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconAI: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPlus: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconClose: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconTrash: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconEdit: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCheck: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSearch: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCopy: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSave: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconEye: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconEyeOff: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconRefresh: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconUpload: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconUndo: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconArrowLeft: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconArrowRight: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconArrowUp: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconArrowDown: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMinus: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconDownload: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconExternal: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconFilters: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChevronDown: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSort: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMore: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChevronUp: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChevronLeft: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChevronRight: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconApps: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMarketing: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChat: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPin: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMenu: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconMoon: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSun: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconLogout: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconChart: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconPercent: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconTrendUp: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconTrendDown: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconStockAdd: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconBalance: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconCalendar: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconBarcode: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconHistory: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconSuccess: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconError: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconWarning: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconAlert: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconInfo: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+declare const IconBlocked: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+/** Girar no consumidor com a classe animate-spin. */
+declare const IconSpinner: {
+    ({ size, className, weight }: IconProps): react.FunctionComponentElement<_phosphor_icons_react.IconProps>;
+    displayName: string;
+};
+
+export { IconAI, IconAddress, IconAlert, IconApps, IconArrowDown, IconArrowLeft, IconArrowRight, IconArrowUp, IconBalance, IconBarcode, IconBlocked, IconCalendar, IconCard, IconCart, IconCatalog, IconChart, IconChat, IconCheck, IconChevronDown, IconChevronLeft, IconChevronRight, IconChevronUp, IconClients, IconClose, IconCommission, IconCompany, IconCopy, IconCourier, IconDashboard, IconDownload, IconEdit, IconError, IconExternal, IconEye, IconEyeOff, IconFilters, IconFinance, IconGift, IconGlobe, IconHistory, IconInfo, IconIntegrations, IconInvoice, IconKey, IconLink, IconLock, IconLogistics, IconLogout, IconMail, IconMarketing, IconMenu, IconMinus, IconMoney, IconMoon, IconMore, IconNotification, IconPeople, IconPercent, IconPhone, IconPin, IconPlus, IconPrinter, IconProducts, IconPromotion, IconProps, IconQrCode, IconReceipt, IconRefresh, IconSales, IconSave, IconSearch, IconSettings, IconShield, IconSort, IconSpinner, IconStockAdd, IconStore, IconSuccess, IconSun, IconSupplier, IconTag, IconTasks, IconTrash, IconTrendDown, IconTrendUp, IconUndo, IconUpload, IconVault, IconWallet, IconWarning };

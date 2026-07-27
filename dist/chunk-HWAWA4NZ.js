ea1c8e3e3a032daf87f7adcf836417de76ff790f
Paulo Martins <jp-zeza@hotmail.com>
2026-07-27 10:49:11 -0400

    release v0.6.2 (dist)

---
+++ dist/chunk-HWAWA4NZ.js
@@ -0,0 +1,11 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// src/lib/utils.ts
+function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+export { cn };
+//# sourceMappingURL=chunk-HWAWA4NZ.js.map
+//# sourceMappingURL=chunk-HWAWA4NZ.js.map
